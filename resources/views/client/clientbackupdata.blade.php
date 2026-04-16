65958ed41ca7c6a21c793bd498724435f9d86fb5
SauravMalakar <sauravmlkr@gmail.com>
2026-04-16 10:57:19 +0600

    Minor Changes in blades and routes

--- resources/views/client/clientbackupdata.blade.php
+++ resources/views/client/clientbackupdata.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <!-- =========================================
-     AHMED HASSAN - VIDEOS
+     Client - VIDEOS
 ========================================= -->
 <section class="services videos-page">
   <div class="videos-hero">
